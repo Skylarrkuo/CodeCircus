d8b82e9249e7ccd8664f3a01180d7fd1e9b9698d
Skylarrkuo <gmc1391368400@outlook.com>
2022-11-14 16:54:23 +0800

    Yang Hui San Jiao

--- C/k6-1.c
+++ C/k6-1.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-
+//杨辉三角
 int main()
 {
     int i, j;
     int a[10][10];
-    for (i = 0; i < 10; i++)
+
+    for (i = 0; i < 10; i++) // i:0~9
     {
-        a[i][0] = 1;
-        for (j = 1; j <= i; j++)
+        for (j = 0; j <= i; j++)
         {
-            if (i > 2&&j>=1)
+            if (i >= 2 && j >= 1)
             {
                 a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
-            }else{
-                a[i][j] = 1;
-                a[i][0] = 1;
             }
-
+            else
+            {
+                a[i][j] = 1;
+            }
             if (i == j)
             {
+                a[i][0] = 1;
                 a[i][j] = 1;
             }
             printf("%d\t", a[i][j]);
