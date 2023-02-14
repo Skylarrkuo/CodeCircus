612f9f2e0e632d9b524608d1a4588496e7fcb4f9
Skylarrkuo <gmc1391368400@outlook.com>
2023-02-14 14:50:32 +0800

    6

--- C/cs.c
+++ C/cs.c
@@ -1,40 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#define N 10 // 定义符号变量
-char str[N];
-void a(char[]);
+int dg(int n)
+{
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        return dg(n - 1) + dg(n - 2);
+}
 int main()
 {
-    // 函数声明
-    int i, j;
-    for (j = 1; j == 1;)
-    // 输入的字符不为10则重新输入
-    { 
-        printf("请用户输入一个字符串：\n");
-        scanf("%s", &str);
-        if (strlen(str) > N)
-            printf("字符串太长，请用户重新输入");
-        else
-            j = 0;
+    int a, i;
+    printf("请输入斐波那契额数列个数");
+    scanf("%d", &a);
+    for (i = 1; i <= a; i++)
+    {
+        if (i % 5 == 0)
+            printf("   ");
+        printf("%3d", dg(i));
     }
-    a(str); // 函数调用
-    printf("排序后的字符串为：\n");
-    for (i = 0; i < N; i++)
-        printf("%c", str[i]);
-    printf("\n");
-    return 0;
-}
-
-void a(char str[])
-{ // 冒泡法排序函数
-    int x, y;
-    char t;
-    for (y = 1; y < N; y++)
-        for (x = 0; (x < N - y) && (str[x] != '\0'); x++)
-            if (str[x] > str[x + 1])
-            {
-                t = str[x];
-                str[x] = str[x + 1];
-                str[x + 1] = t;
-            }
 }
