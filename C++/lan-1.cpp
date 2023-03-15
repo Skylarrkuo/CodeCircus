73c7f00d6ee2bce5a2c109808b2f710f7a11d4e6
Skylarrkuo <gmc1391368400@outlook.com>
2023-03-15 13:01:20 +0800

    6

---
+++ C++/lan-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ 
+// main() 是程序开始执行的地方
+ 
+int main()
+{
+   cout << "Hello World\n"; // 输出 Hello World
+   return 0;
+}
--- Java/sc/Hello.java
+++ Java/sc/Hello.java
@@ -1,6 +1,13 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World"); // 输出 Hello World
-    }
-
+public class Hello{
+		public static void main(String args[]){
+			System.out.println("大家好！");
+			System.out.println("Nice to meet you");
+			Student stu = new Student();
+			stu.speak("We are students");
+		}
+}
+class Student{
+	public void speak(String s){
+		System.out.println(s);
+	}
 }
