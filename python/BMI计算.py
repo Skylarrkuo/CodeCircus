4350d2a8147921d213d2fe1b0bfcd494de9ffab0
skylarrkuo <gmc1391368400@outlook.com>
2023-11-02 08:40:29 +0800

    up

--- python/BMI计算.py
+++ python/BMI计算.py
@@ -3,6 +3,7 @@ import math
 
 bmi = 0.0
 who=0
+whoch=0
 Leib=["偏瘦","正常","肥胖","偏胖"]
 
 height = eval(input("输入身高："))
@@ -10,7 +11,7 @@ weight = eval(input("输入体重："))
 bmi = height / math.pow(weight, 2)
 
 if(bmi<18.5):
-    who=0
+    who=whocn=0
 elif(18.5 <= bmi<=25):
     who=1
 elif(25<bmi<30):
