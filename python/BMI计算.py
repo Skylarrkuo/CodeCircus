5034fed5ba36e4b860cc738bb68a7e12046fa582
skylarrkuo <gmc1391368400@outlook.com>
2023-11-02 13:12:16 +0800

    up

--- python/BMI计算.py
+++ python/BMI计算.py
@@ -2,18 +2,33 @@
 import math
 
 bmi = 0.0
-who=0
-whoch=0
-Leib=["偏瘦","正常","肥胖","偏胖"]
+who = 0
+whoch = 0
+Leib = ["偏瘦", "正常", "肥胖", "偏胖"]
 
-height = eval(input("输入身高："))
-weight = eval(input("输入体重："))
-bmi = height / math.pow(weight, 2)
+height = eval(input("输入身高："))  # 输入身高
+weight = eval(input("输入体重："))  # 输入体重
+bmi = weight / math.pow(height, 2)  # 计算BMI值
+print("Your BMI={:.2f}".format(bmi))  # 输出BMI值
 
-if(bmi<18.5):
-    who=whocn=0
-elif(18.5 <= bmi<=25):
-    who=1
-elif(25<bmi<30):
-    who=2
-elif()
+# 根据BMI值判断身体状态
+if bmi < 18.5:
+    who = whocn = 0
+elif 18.5 <= bmi <= 25:
+    who = 1
+    if bmi >= 24:
+        whoch = 2
+    else:
+        whoch = 1
+elif 25 < bmi < 30:
+    who = 2
+    if bmi >= 28:
+        whoch = 3
+    else:
+        whoch = 2
+elif bmi >= 30:
+    who = whocn = 3
+
+# 输出身体状态
+print("你的身体：")
+print("国际：{}; 国内：{}".format(Leib[who], Leib[whoch]))
