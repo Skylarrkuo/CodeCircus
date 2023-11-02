4b97cdf0d1e993ad5b9d45176d673d72ebd40c3b
skylarrkuo <gmc1391368400@outlook.com>
2023-11-02 08:37:03 +0800

    up

---
+++ C++/哈夫曼树/Hafmanjosn.cpp
@@ -0,0 +1,94 @@
+#include <iostream> //读写io c++标准库
+#include <fstream> //读写文件 c++标准库
+#include <string> //字符串类 c++标准库
+#include <sstream> //字符串流 c++标准库
+#include "json/json.h" //jsoncpp的头文件using namespace std;
+
+using namespace std;
+
+typedef struct
+{
+    int weight;
+    int parent, lchild, rchild;
+} HTNode, *HuffmanTree;
+
+void Select(HuffmanTree &HT, int end, int &s1, int &s2)
+{
+    int min1, min2;
+    // 初始化最小和次小权重
+    min1 = min2 = INT_MAX;
+    // 初始化最小和次小权重对应的索引
+    s1 = s2 = 0;
+    for (int i = 1; i <= end; ++i)
+    {
+        if (HT[i].parent == 0)
+        {
+            if (HT[i].weight < min1)
+            {
+                min2 = min1;
+                s2 = s1;
+                min1 = HT[i].weight;
+                s1 = i;
+            }
+            else if (HT[i].weight < min2)
+            {
+                min2 = HT[i].weight;
+                s2 = i;
+            }
+        }
+    }
+}
+
+void CreateHuffmanTree(HuffmanTree &HT, int n)
+{ // 构造哈夫曼树HT
+    if (n <= 1)
+        return;
+    int m = 2 * n - 1;
+    HT = new HTNode[m + 1];
+    for (int i = 1; i <= m; i++)
+    {
+        HT[i].parent = 0;
+        HT[i].lchild = 0;
+        HT[i].rchild = 0;
+    }
+    for (int i = 1; i <= n; ++i)
+        cin >> HT[i].weight;
+    for (int i = n + 1; i <= m; ++i)
+    { // 通过n-1次的选择、删除、合并来创建哈夫曼树
+        int s1, s2;
+        Select(HT, i - 1, s1, s2);
+        HT[s1].parent = i;
+        HT[s2].parent = i;
+        HT[i].lchild = s1;
+        HT[i].rchild = s2;
+        HT[i].weight = HT[s1].weight + HT[s2].weight;
+    }
+}
+
+Json::Value HuffmanTreeToJson(HuffmanTree &HT, int i)
+{
+    Json::Value root;
+    root["weight"] = HT[i].weight;
+    if (HT[i].lchild != 0)
+        root["left"] = HuffmanTreeToJson(HT, HT[i].lchild);
+    if (HT[i].rchild != 0)
+        root["right"] = HuffmanTreeToJson(HT, HT[i].rchild);
+    return root;
+}
+
+int main()
+{
+    HuffmanTree HT;
+    int n = 5; // 假设有5个叶子节点
+    CreateHuffmanTree(HT, n);
+    
+    Json::Value root = HuffmanTreeToJson(HT, 2 * n - 1);
+    Json::StreamWriterBuilder writerBuilder;
+    std::string output = Json::writeString(writerBuilder, root);
+    
+    cout << output << endl;
+    
+    delete[] HT;
+    
+    return 0;
+}
--- python/BMI计算.py
+++ python/BMI计算.py
@@ -2,6 +2,17 @@
 import math
 
 bmi = 0.0
+who=0
+Leib=["偏瘦","正常","肥胖","偏胖"]
+
 height = eval(input("输入身高："))
 weight = eval(input("输入体重："))
 bmi = height / math.pow(weight, 2)
+
+if(bmi<18.5):
+    who=0
+elif(18.5 <= bmi<=25):
+    who=1
+elif(25<bmi<30):
+    who=2
+elif()
