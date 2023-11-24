ebd9fc3c5e1a6bb8afab098f2cc3c5ddfbbb9100
skylarrkuo <gmc1391368400@outlook.com>
2023-11-24 09:31:01 +0800

    up

--- 汇编/sy2/1.asm
+++ 汇编/sy2/1.asm
@@ -1,95 +1,95 @@
 DATAS SEGMENT
 	; 数据段声明
-	in_msg  db 'input hex: $'    ; 输入提示消息
-	out_msg db 10,13,'output: $'  ; 输出提示消息
-	inv     db 10,13,'invalid  $' ; 无效输入消息
+	in_msg  db 'input hex: $'    	; 输入提示消息
+	out_msg db 10,13,'output: $' 	; 输出提示消息
+	inv     db 10,13,'invalid  $'	; 无效输入消息
 DATAS ENDS
 
 STACKS SEGMENT
-	; 堆栈段声明（此代码中没有堆栈操作）
+	; 堆栈段声明(此代码中没有堆栈操作)
 STACKS ENDS
 
 CODES SEGMENT
-	ASSUME CS:CODES,DS:DATAS,SS:STACKS  ; 设置段寄存器
+	      ASSUME CS:CODES,DS:DATAS,SS:STACKS	; 设置段寄存器
 
-START:
-	MOV AX,DATAS    ; 将数据段地址加载到AX寄存器
-	MOV DS,AX       ; 将数据段寄存器DS设置为AX的值
+	START:
+	      MOV    AX,DATAS                   	; 将数据段地址加载到AX寄存器
+	      MOV    DS,AX                      	; 将数据段寄存器DS设置为AX的值
 
 	; 输入代码段
-	lea    dx,in_msg  ; 将输入消息的有效地址加载到DX寄存器
-	mov    ah,09       ; 设置中断功能为打印字符串
-	int    21h         ; 调用中断21h以打印输入消息
+	      lea    dx,in_msg                  	; 将输入消息的有效地址加载到DX寄存器
+	      mov    ah,09                      	; 设置中断功能为打印字符串
+	      int    21h                        	; 调用中断21h以打印输入消息
     
-	xor    bx,bx       ; 清除BX寄存器以进行计算
+	      xor    bx,bx                      	; 清除BX寄存器以进行计算
 
 	l0:   
-	mov    ah,1        ; 设置中断功能为读取字符
-	int    21h         ; 调用中断21h以读取字符
-	cmp    al,0dh      ; 检查输入的字符是否为回车（Enter）
-	je     l4          ; 如果是，跳转到标签l4（输入结束）
-	cmp    al,'0'       ; 将输入的字符与'0'比较
-	jb     l5          ; 如果小于，跳转到标签l5（无效输入）
-	cmp    al,'9'       ; 将输入的字符与'9'比较
-	ja     l1          ; 如果大于，跳转到标签l1
-	sub    al,30h      ; 将ASCII字符转换为相应的数值
-	jmp    l3          ; 跳转到标签l3
+	      mov    ah,1                       	; 设置中断功能为读取字符
+	      int    21h                        	; 调用中断21h以读取字符
+	      cmp    al,0dh                     	; 检查输入的字符是否为回车(Enter)
+	      je     l4                         	; 如果是,跳转到标签l4(输入结束)
+	      cmp    al,'0'                     	; 将输入的字符与'0'比较
+	      jb     l5                         	; 如果小于,跳转到标签l5(无效输入)
+	      cmp    al,'9'                     	; 将输入的字符与'9'比较
+	      ja     l1                         	; 如果大于,跳转到标签l1
+	      sub    al,30h                     	; 将ASCII字符转换为相应的数值
+	      jmp    l3                         	; 跳转到标签l3
 
-	l1:   cmp    al,'A'       ; 将输入的字符与'A'比较
-	jb     l5          ; 如果小于，跳转到标签l5（无效输入）
-	cmp    al,'F'       ; 将输入的字符与'F'比较
-	ja     l2          ; 如果大于，跳转到标签l2
-	sub    al,37h      ; 将ASCII字符转换为相应的数值
-	jmp    l3          ; 跳转到标签l3
+	l1:   cmp    al,'A'                     	; 将输入的字符与'A'比较
+	      jb     l5                         	; 如果小于,跳转到标签l5(无效输入)
+	      cmp    al,'F'                     	; 将输入的字符与'F'比较
+	      ja     l2                         	; 如果大于,跳转到标签l2
+	      sub    al,37h                     	; 将ASCII字符转换为相应的数值
+	      jmp    l3                         	; 跳转到标签l3
 
-	l2:   cmp    al,'a'       ; 将输入的字符与'a'比较
-	jb     l5          ; 如果小于，跳转到标签l5（无效输入）
-	cmp    al,'f'       ; 将输入的字符与'f'比较
-	ja     l5          ; 如果大于，跳转到标签l5（无效输入）
-	sub    al,57h      ; 将ASCII字符转换为相应的数值
+	l2:   cmp    al,'a'                     	; 将输入的字符与'a'比较
+	      jb     l5                         	; 如果小于,跳转到标签l5(无效输入)
+	      cmp    al,'f'                     	; 将输入的字符与'f'比较
+	      ja     l5                         	; 如果大于,跳转到标签l5(无效输入)
+	      sub    al,57h                     	; 将ASCII字符转换为相应的数值
 
-	l3:   mov    ah,0        ; 清除AH寄存器
-	push   cx            ; 将CX寄存器的值保存到堆栈上
-	mov    cl,4          ; 将CL寄存器设置为4，用于位操作
-	shl    bx,cl         ; 将BX寄存器左移4位
-	add    bx,ax         ; 将转换后的数值加到BX寄存器
-	pop    cx            ; 从堆栈上恢复CX寄存器的值
-	loop   l0            ; 循环回到标签l0，以读取下一个字符
+	l3:   mov    ah,0                       	; 清除AH寄存器
+	      push   cx                         	; 将CX寄存器的值保存到堆栈上
+	      mov    cl,4                       	; 将CL寄存器设置为4,用于位操作
+	      shl    bx,cl                      	; 将BX寄存器左移4位
+	      add    bx,ax                      	; 将转换后的数值加到BX寄存器
+	      pop    cx                         	; 从堆栈上恢复CX寄存器的值
+	      loop   l0                         	; 循环回到标签l0,以读取下一个字符
 
 	l4:   
-	mov    dx,offset out_msg  ; 将输出消息的有效地址加载到DX寄存器
-	mov    ah,9            ; 设置中断功能为打印字符串
-	int    21h             ; 调用中断21h以打印输出消息
-	mov    cx,16           ; 将CX寄存器设置为16（位数）
-	mov    ax,8000h        ; 将AX寄存器设置为8000h（二进制：1000000000000000）
+	      mov    dx,offset out_msg          	; 将输出消息的有效地址加载到DX寄存器
+	      mov    ah,9                       	; 设置中断功能为打印字符串
+	      int    21h                        	; 调用中断21h以打印输出消息
+	      mov    cx,16                      	; 将CX寄存器设置为16(位数)
+	      mov    ax,8000h                   	; 将AX寄存器设置为8000h(二进制:1000000000000000)
 
 	l8:   
-	push   ax               ; 将AX寄存器的值保存到堆栈上
-	test   bx,ax            ; 测试BX的最左边的位是否设置
-	jnz    l9               ; 如果是，跳转到标签l9
-	mov    dl,'0'           ; 如果不是，将DL寄存器设置为'0'
-	jmp    l10              ; 跳转到标签l10
+	      push   ax                         	; 将AX寄存器的值保存到堆栈上
+	      test   bx,ax                      	; 测试BX的最左边的位是否设置
+	      jnz    l9                         	; 如果是,跳转到标签l9
+	      mov    dl,'0'                     	; 如果不是,将DL寄存器设置为'0'
+	      jmp    l10                        	; 跳转到标签l10
 
 	l9:   
-	mov    dl,'1'           ; 将DL寄存器设置为'1'
+	      mov    dl,'1'                     	; 将DL寄存器设置为'1'
 
 	l10:  
-	mov    ah,2            ; 设置中断功能为打印字符
-	int    21h             ; 调用中断21h以打印字符
-	pop    ax               ; 从堆栈上恢复AX寄存器的值
-	shr    ax,1            ; 将AX寄存器右移1位
-	loop   l8              ; 循环回到标签l8，以打印下一个位
+	      mov    ah,2                       	; 设置中断功能为打印字符
+	      int    21h                        	; 调用中断21h以打印字符
+	      pop    ax                         	; 从堆栈上恢复AX寄存器的值
+	      shr    ax,1                       	; 将AX寄存器右移1位
+	      loop   l8                         	; 循环回到标签l8,以打印下一个位
 
-	jmp    l6              ; 跳转到标签l6（程序结束）
+	      jmp    l6                         	; 跳转到标签l6(程序结束)
 
 	l5:   
-	mov    ah,9            ; 设置中断功能为打印字符串
-	mov    dx,offset inv   ; 将无效输入消息的有效地址加载到DX寄存器
-	int    21h             ; 调用中断21h以打印无效输入消息
+	      mov    ah,9                       	; 设置中断功能为打印字符串
+	      mov    dx,offset inv              	; 将无效输入消息的有效地址加载到DX寄存器
+	      int    21h                        	; 调用中断21h以打印无效输入消息
 
 	l6:   
-	MOV    AH,4CH          ; 设置中断功能为终止程序
-	INT    21H             ; 调用中断21h以终止程序
+	      MOV    AH,4CH                     	; 设置中断功能为终止程序
+	      INT    21H                        	; 调用中断21h以终止程序
 
 CODES ENDS
 END START
--- 汇编/sy2/2.asm
+++ 汇编/sy2/2.asm
@@ -3,7 +3,7 @@ DATAS SEGMENT
       OPR1  DB -85,85,85,85,-85,-85
       OP    DB '++----'
       OPR2  DB 76,-76,76,-76,76,-76
-      RES   DB '(1)CF='                  ;完成一个运算式的计算后，要将编号加1
+      RES   DB '(1)CF='                  ;完成一个运算式的计算后,要将编号加1
       CF    DB '0'                       ;根据运算结果将CF标志的值填写在这里
             DB ',ZF='
       ZF    DB '0'                       ;根据运算结果将ZF标志的值填写在这里
@@ -11,7 +11,7 @@ DATAS SEGMENT
       SF    DB '0'                       ;根据运算结果将SF标志的值填写在这里
             DB ',OF='
       OF    DB '0'                       ;根据运算结果将OF标志的值填写在这里
-            DB 0DH,0AH,'$'               ;回车换行，字符串结束，控制结果分行显示
+            DB 0DH,0AH,'$'               ;回车换行,字符串结束,控制结果分行显示
 DATAS ENDS
 
 STACKS SEGMENT
@@ -26,11 +26,11 @@ CODES SEGMENT
       ;此处输入代码段代码
             XOR    SI,SI                            ;将数组下标寄存器清0
       L0:   
-      ;判断OP[SI]的值是‘+’还是‘-’来决定是执行加法还是减法运算
+      ;判断OP[SI]的值是'+'还是'-'来决定是执行加法还是减法运算
             MOV    AL,OPR1[SI]
             MOV    BL,OP[SI]
             CMP    BL,'+'
-            JNZ    L1                               ;如果不是+则跳转，如果是+则跳过
+            JNZ    L1                               ;如果不是+则跳转,如果是+则跳过
             ADD    AL,OPR2[SI]                      ;加法运算
             JMP    L2
       L1:   SUB    AL,OPR2[SI]                      ;减法运算
@@ -43,12 +43,12 @@ CODES SEGMENT
             MOV    ZF,'0'
             JMP    L6
       L5:   MOV    ZF,'1'
-      L6:   JS     L7                               ;JS（Jump if Sign）指令，它会在SF为1时跳转
-            MOV    SF,'0'                           ;SF标志表示运算结果的符号，如果结果为负数，则SF被设置为1，否则为0。
+      L6:   JS     L7                               ;JS(Jump if Sign)指令,它会在SF为1时跳转
+            MOV    SF,'0'                           ;SF标志表示运算结果的符号,如果结果为负数,则SF被设置为1,否则为0.
             JMP    L8
       L7:   MOV    SF,'1'
-      L8:   JO     L9                               ;JO（Jump if Overflow）指令，它会在OF为1时跳转
-            MOV    OF,'0'                           ;OF标志表示运算结果是否溢出，如果溢出则为1，否则为0。
+      L8:   JO     L9                               ;JO(Jump if Overflow)指令,它会在OF为1时跳转
+            MOV    OF,'0'                           ;OF标志表示运算结果是否溢出,如果溢出则为1,否则为0.
             JMP    L10
       L9:   MOV    OF,'1'
       L10:  MOV    AH,09H                           ;将显示字符串的功能号送AH
